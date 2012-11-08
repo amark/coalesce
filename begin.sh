584f7a8c1659948277076b541a7478a818635826
Joshua Bell <pensivearchitect@gmail.com>
2012-11-07 23:44:28 -0800

    Cleaning up, still needs to be thought out more

--- begin.sh
+++ begin.sh
@@ -4,19 +4,16 @@
 
 
 #determine kernel
-if `uname | grep darwin` !== true;
-
 #Operating System is a Linux/Unix
 	if apt-cache search bash = true;
 #Debian-Based
-	sudo apt-get update; sudo apt-get upgrade; sudo apt-get install wget curl zsh git-core build-essential openssl libssl-dev upstart monit libtool autoconf uuid-dev mdadm lvm2 xfsprogs;
+then	sudo apt-get update; sudo apt-get upgrade; sudo apt-get install wget curl zsh git-core build-essential openssl libssl-dev upstart monit libtool autoconf uuid-dev mdadm lvm2 xfsprogs;
 
-	fi;
 
-fi;
 
-if `uname | grep darwin` !== true;
 
-	ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"; brew update; brew install openssl dpkg autoconf node mongodb redis redis-tools phantomjs czmq;
+if `uname | grep darwin` = true;
 
-./node index.js;
+then ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"; brew update; brew install openssl dpkg autoconf node mongodb redis redis-tools phantomjs czmq;
+
+node index.js;
