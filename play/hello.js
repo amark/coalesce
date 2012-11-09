043d7e0b8ca7ddfa15601454a58fb52a61618f95
mark <theory@theory.(none)>
2012-11-09 07:17:20 -0800

    example bump

--- play/hello.js
+++ play/hello.js
@@ -3,7 +3,7 @@ module.exports = require('theory')
 
     a.com.send({ what: "World", where: {on: 'magic'} });
 
-    return (document.hello.to.onkeyup = function(m){
+    return (document.onkeyup = function(m){
 	
 		m.what? document.hello.to.value = m.what : a.com.send({ 
 		what: 	document.hello.to.value, where: 'magic' });
